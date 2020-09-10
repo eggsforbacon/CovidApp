36032f9fc378804d702fc36ed9845aad4b190377
Samuel Hernandez <doicirohangnan@gmail.com>
2020-09-10 09:15:05 -0500

    Added new ui class to leave main class for mai  method only

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,7 @@ package ui;
 import java.util.*;
 import java.io.IOException;
 import model.*;
+import ui.Validations;
 public class Main {
 
   public static void main(String[] args) {
@@ -36,8 +37,8 @@ public class Main {
           System.out.println("Ingrese la cantidad de materiales distintos solicitados:\n");
           int matCount = in.nextInt();
           for (int i = 0; i < matCount; i++) {
-            matNamesUnit = inputNames(i,in,methNester,matCount,matNamesUnit);
-            quantityPurpose = quantityType(i,in,methNester,matCount,quantityPurpose);
+            matNamesUnit = Validations.inputNames(i,in,methNester,matCount,matNamesUnit);
+            quantityPurpose = Validations.quantityType(i,in,methNester,matCount,quantityPurpose);
           }
           break;
         default:
@@ -47,37 +48,6 @@ public class Main {
 
   }
 
-  public static String[][] inputNames(int i,Scanner in, Wires methNester, int matCount, String[][] matNamesUnit) {
-    matNamesUnit = new String[2][matCount];
-    methNester.clsm();
-    System.out.println("************************************************");
-    System.out.println("Ingrese el NOMBRE del siguiente material seguido de su UNIDAD DE MEDIDA\n");
-    matNamesUnit[0][i] = in.next();
-    in.nextLine();
-    matNamesUnit[0][i] = matNamesUnit[0][i].toLowerCase();
-    System.out.println("************************************************");
-    matNamesUnit[1][i] = in.nextLine();
-    matNamesUnit[1][i] = matNamesUnit[1][i].toLowerCase();
-    methNester.clsm();
-    return matNamesUnit;
-  }
+  //Here
 
-  public static int[][] quantityType(int i, Scanner in, Wires methNester, int matCount, int[][] quantityPurpose) {
-    quantityPurpose = new int[2][matCount];
-    methNester.clsm();
-    System.out.println("************************************************");
-    System.out.println("Ingrese la CANTIDAD del material solicitado\n");
-    quantityPurpose[0][i] = in.nextInt();
-    methNester.clsm();
-    System.out.println("Ingrese el PROPOSITO [1 = Obra Negra; 2 = Obra Blanca; 3 = Pintura] del material solicitado\n");
-    quantityPurpose[1][i] = in.nextInt();
-    return quantityPurpose;
-  }
-     /*
-    matPurpose = new int[matCount];
-    for (int i = 0; i < matCount; i++) {
-
-    matPurpose[i] = in.nextInt();
-      System.out.println("Material registrado con exito!");
-    }*/
 }
